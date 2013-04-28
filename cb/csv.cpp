1cca30e8f7b35f57ed9fd91e79d6d13e640eac91
Jim McDonald <Jim@mcdee.net>
2013-04-28 21:58:30 +0100

    Select first and last block to dump

--- cb/csv.cpp
+++ cb/csv.cpp
@@ -8,7 +8,6 @@
 #include <option.h>
 #include <callback.h>
 
-static uint8_t empty[kSHA256ByteSize] = { 0x42 };
 typedef GoogMap<Hash256, uint64_t, Hash256Hasher, Hash256Equal>::Map OutputMap;
 
 struct CSVDump:public Callback
@@ -18,17 +17,21 @@ struct CSVDump:public Callback
     FILE *inputFile;
     FILE *outputFile;
 
-    uint64_t txID;
-    uint64_t blkID;
-    int64_t cutoffBlock;
     optparse::OptionParser parser;
+    int64_t firstBlock;
+    int64_t lastBlock;
+
+    uint32_t active;
 
     // Counters used across functions
+    uint64_t blkID;
     uint64_t blkSize;
-    uint64_t totalBlkInput;
+    uint64_t totalBlkFees;
     uint64_t totalBlkOutput;
     uint64_t numBlkTxs;
 
+    uint64_t txID;
+    uint32_t genTx;
     const uint8_t *txStart;
     uint64_t numTxInputs;
     uint64_t numTxOutputs;
@@ -44,11 +47,18 @@ struct CSVDump:public Callback
             .epilog("")
         ;
         parser
-            .add_option("-a", "--atBlock")
+            .add_option("-f", "--firstBlock")
+            .action("store")
+            .type("int")
+            .set_default(0)
+            .help("first block to dump (default: 0)")
+        ;
+        parser
+            .add_option("-l", "--lastBlock")
             .action("store")
             .type("int")
             .set_default(-1)
-            .help("stop dump at block <block> (default: all)")
+            .help("last block to dump (default: last block)")
         ;
     }
 
@@ -70,11 +80,13 @@ struct CSVDump:public Callback
     {
         txID = 0;
         blkID = 0;
+        active = 0;
 
         optparse::Values &values = parser.parse_args(argc, argv);
-        cutoffBlock = values.get("atBlock");
+        firstBlock = values.get("firstBlock");
+        lastBlock = values.get("lastBlock");
 
-        info("Dumping the blockchain ...");
+        info("Dumping the blockchain...");
 
         blockFile = fopen("blocks.csv", "w");
         if(!blockFile) sysErrFatal("couldn't open file blocks.csv for writing\n");
@@ -100,90 +112,96 @@ struct CSVDump:public Callback
         uint64_t chainBase
     )
     {
-        if(0<=cutoffBlock && cutoffBlock<b->height) wrapup();
+        if (lastBlock >= 0 && lastBlock < b->height - 1) wrapup();
+        if (b->height - 1 == firstBlock) active = 1;
 
-        uint8_t blockHash[kSHA256ByteSize];
-        sha256Twice(blockHash, b->data, 80);
+        if (active) {
+            uint8_t blockHash[kSHA256ByteSize];
+            sha256Twice(blockHash, b->data, 80);
 
-        const uint8_t *p = b->data;
+            const uint8_t *p = b->data;
 
-        // Total block size is 81 bytes plus transactions
-        blkSize = 81;
+            // Total block size is 81 bytes plus transactions
+            blkSize = 81;
 
-        totalBlkInput = 0;
-        totalBlkOutput = 0;
-        numBlkTxs = 0;
+            totalBlkFees = 0;
+            totalBlkOutput = 0;
+            numBlkTxs = 0;
 
-        LOAD(uint32_t, version, p);
-        SKIP(uint256_t, prevBlkHash, p);
-        LOAD(uint256_t, blkMerkleRoot, p);
-        LOAD(uint32_t, blkTime, p);
-        LOAD(uint32_t, difficultyBits, p);
-        LOAD(uint32_t, nonce, p);
+            LOAD(uint32_t, version, p);
+            SKIP(uint256_t, prevBlkHash, p);
+            LOAD(uint256_t, blkMerkleRoot, p);
+            LOAD(uint32_t, blkTime, p);
+            LOAD(uint32_t, difficultyBits, p);
+            LOAD(uint32_t, nonce, p);
 
-        // ID
-        fprintf(blockFile, "%" PRIu64 ",", blkID);
+            // ID
+            fprintf(blockFile, "%" PRIu64 ",", blkID);
 
-        // Hash
-        uint8_t buf[1 + 2*kSHA256ByteSize];
-        toHex(buf, blockHash);
-        fprintf(blockFile, "\"%s\",", buf);
+            // Hash
+            uint8_t buf[1 + 2*kSHA256ByteSize];
+            toHex(buf, blockHash);
+            fprintf(blockFile, "\"%s\",", buf);
 
-        // Version
-        fprintf(blockFile, "%" PRIu32 ",", version);
+            // Version
+            fprintf(blockFile, "%" PRIu32 ",", version);
 
-        // Timestamp
-        time_t blockTime = blkTime;
-        char tbuf[23];
-        strftime(tbuf, sizeof tbuf, "%FT%TZ", gmtime(&blockTime));
+            // Timestamp
+            time_t blockTime = blkTime;
+            char tbuf[23];
+            strftime(tbuf, sizeof tbuf, "%FT%TZ", gmtime(&blockTime));
 
-        fprintf(blockFile, "\"%s\",", tbuf);
+            fprintf(blockFile, "\"%s\",", tbuf);
 
-        // Nonce
-        fprintf(blockFile, "%" PRIu32 ",", nonce);
+            // Nonce
+            fprintf(blockFile, "%" PRIu32 ",", nonce);
 
-        // Difficulty
-        fprintf(blockFile, "%f,", difficulty(difficultyBits));
+            // Difficulty
+            fprintf(blockFile, "%f,", difficulty(difficultyBits));
 
-        // Merkle root
-        uint8_t buf2[kSHA256ByteSize];
-        uint8_t buf3[1 + 2*kSHA256ByteSize];
-        memcpy(buf2, &blkMerkleRoot, kSHA256ByteSize);
-        toHex(buf3, buf2);
-        fprintf(blockFile, "\"%s\",", buf3);
+            // Merkle root
+            uint8_t buf2[kSHA256ByteSize];
+            uint8_t buf3[1 + 2*kSHA256ByteSize];
+            memcpy(buf2, &blkMerkleRoot, kSHA256ByteSize);
+            toHex(buf3, buf2);
+            fprintf(blockFile, "\"%s\",", buf3);
+        }
     }
 
     virtual void endBlock(
         const Block *b
     )
     {
-        // Number of transactions
-        fprintf(blockFile, "%" PRIu64 ",", numBlkTxs);
+        if (active)
+        {
+            // Number of transactions
+            fprintf(blockFile, "%" PRIu64 ",", numBlkTxs);
 
-        // Value of output transactions
-        fprintf(blockFile, "%" PRIu64 ",", totalBlkOutput);
+            // Value of output transactions
+            fprintf(blockFile, "%" PRIu64 ",", totalBlkOutput);
 
-        // Value of fees
-        fprintf(blockFile, "%" PRIu64 ",", totalBlkInput - totalBlkOutput);
+            // Value of fees
+            fprintf(blockFile, "%" PRIu64 ",", totalBlkFees);
 
-        // Size; depends on number of transactions
-        if (numBlkTxs < 253)
-        {
-            // Nothing to do
+            // Size; depends on number of transactions
+            if (numBlkTxs < 253)
+            {
+                // Nothing to do
+            }
+            else if (numBlkTxs < 65536)
+            {
+                blkSize += 2;
+            }
+            else if (numBlkTxs < 4294967296L)
+            {
+                blkSize += 4;
+            }
+            else
+            {
+                blkSize += 8;
+            }
+            fprintf(blockFile, "%" PRIu64 "\n", blkSize);
         }
-        else if (numBlkTxs < 65536)
-        {
-            blkSize += 2;
-        }
-        else if (numBlkTxs < 4294967296L)
-        {
-            blkSize += 4;
-        }
-        else
-        {
-            blkSize += 8;
-        }
-        fprintf(blockFile, "%" PRIu64 "\n", blkSize);
 
         blkID++;
     }
@@ -193,58 +211,70 @@ struct CSVDump:public Callback
         const uint8_t *hash
     )
     {
-        txStart = p;
-        numBlkTxs++;
-        numTxInputs = 0;
-        numTxOutputs = 0;
-        totalTxInput = 0;
-        totalTxOutput = 0;
+        if (active)
+        {
+            txStart = p;
+            numBlkTxs++;
+            numTxInputs = 0;
+            numTxOutputs = 0;
+            totalTxInput = 0;
+            totalTxOutput = 0;
 
-        // ID
-        fprintf(txFile, "%" PRIu64 ",", txID);
+            // ID
+            fprintf(txFile, "%" PRIu64 ",", txID);
 
-        // Hash
-        uint8_t buf[1 + 2*kSHA256ByteSize];
-        toHex(buf, hash);
-        fprintf(txFile, "\"%s\",", buf);
+            // Hash
+            uint8_t buf[1 + 2*kSHA256ByteSize];
+            toHex(buf, hash);
+            fprintf(txFile, "\"%s\",", buf);
 
-        // Version
-        LOAD(uint32_t, version, p);
-        fprintf(txFile, "%" PRIu32 ",", version);
+            // Version
+            LOAD(uint32_t, version, p);
+            fprintf(txFile, "%" PRIu32 ",", version);
 
-
-        // Block ID
-        fprintf(txFile, "%" PRIu64 ",", blkID);
+            // Block ID
+            fprintf(txFile, "%" PRIu64 ",", blkID);
+        }
     }
 
     virtual void endTX(
         const uint8_t *p
     )
     {
-        // Number of inputs
-        fprintf(txFile, "%" PRIu64 ",", numTxInputs);
+        if (active)
+        {
+            // Number of inputs
+            fprintf(txFile, "%" PRIu64 ",", numTxInputs);
 
-        // Number of outputs
-        fprintf(txFile, "%" PRIu64 ",", numTxOutputs);
+            // Number of outputs
+            fprintf(txFile, "%" PRIu64 ",", numTxOutputs);
 
-        // Value of outputs
-        fprintf(txFile, "%" PRIu64 ",", totalTxOutput);
+            // Value of outputs
+            fprintf(txFile, "%" PRIu64 ",", totalTxOutput);
 
-        // Value of fees
-        // TODO Fix this
-//        fprintf(txFile, "%" PRIu64 ",", totalTxInput - totalTxOutput);
-        fprintf(txFile, "%" PRIu64 ",", 0);
+            // Value of fees
+            if (genTx)
+            {
+                fprintf(txFile, "0,");
+            }
+            else
+            {
+                fprintf(txFile, "%" PRIu64 ",", totalTxInput - totalTxOutput);
+                totalBlkFees += totalTxInput - totalTxOutput;
+            }
 
-        // Lock time
-        LOAD(uint32_t, lockTime, p);
-        fprintf(txFile, "%" PRIu32 ",", lockTime);
+            // Lock time
+            LOAD(uint32_t, lockTime, p);
+            fprintf(txFile, "%" PRIu32 ",", lockTime);
 
-        // Size
-        // p is 4 bigger than it should be due to above lock time load
-        uint64_t txSize = p - 4 - txStart;
-        blkSize += txSize;
-        fprintf(txFile, "%" PRIu64 "\n", txSize);
+            // Size
+            // p is 4 bigger than it should be due to above lock time load
+            uint64_t txSize = p - 4 - txStart;
+            blkSize += txSize;
+            fprintf(txFile, "%" PRIu64 "\n", txSize);
 
+            totalBlkOutput += totalTxOutput;
+        }
         txID++;
     }
 
@@ -257,37 +287,46 @@ struct CSVDump:public Callback
         uint64_t      outputScriptSize
     )
     {
-        numTxOutputs++;
-        totalTxOutput += value;
-        totalBlkOutput += value;
+        if (active)
+        {
+            numTxOutputs++;
+            totalTxOutput += value;
 
-        // Script
-        uint8_t script[1 + 2*outputScriptSize];
-        toHex(script, outputScript, outputScriptSize);
+            // Script
+            uint8_t script[1 + 2*outputScriptSize];
+            toHex(script, outputScript, outputScriptSize);
 
-        // Receiving address
-        uint8_t address[40];
-        address[0] = 'X';
-        address[1] = 0;
-        uint8_t addrType[3];
-        uint160_t pubKeyHash;
-        int type = solveOutputScript(pubKeyHash.v, outputScript, outputScriptSize, addrType);
-        if(likely(0<=type)) hash160ToAddr(address, pubKeyHash.v);
+            // Receiving address
+            uint8_t address[40];
+            address[0] = 'X';
+            address[1] = 0;
+            uint8_t addrType[3];
+            uint160_t pubKeyHash;
+            int type = solveOutputScript(pubKeyHash.v, outputScript, outputScriptSize, addrType);
+            if(likely(0<=type)) hash160ToAddr(address, pubKeyHash.v);
 
-        // N.B. Input hash and index are NULL at this stage
-        fprintf(outputFile, "%" PRIu64 ",%" PRIu64 ",%" PRIu64 ",\"%s\",\"%s\",,\n", txID, outputIndex, value, script, address);
+            // N.B. Input hash and index are NULL at this stage
+            fprintf(outputFile, "%" PRIu64 ",%" PRIu64 ",%" PRIu64 ",\"%s\",\"%s\",,\n", txID, outputIndex, value, script, address);
+        }
     }
 
     virtual void startInput(
         const uint8_t *p
     )
     {
-        static uint256_t gNullHash;
-        LOAD(uint256_t, upTXHash, p);
-        if (0==memcmp(gNullHash.v, upTXHash.v, sizeof(gNullHash))) {
-            uint64_t reward = getBaseReward(blkID);
-            totalTxInput += reward;
-            totalBlkInput += reward;
+        if (active)
+        {
+            static uint256_t gNullHash;
+            LOAD(uint256_t, upTXHash, p);
+            if (0==memcmp(gNullHash.v, upTXHash.v, sizeof(gNullHash))) {
+                genTx = 1;
+                uint64_t reward = getBaseReward(blkID);
+                totalTxInput += reward;
+            }
+            else
+            {
+                genTx = 0;
+            }
         }
     }
 
@@ -304,19 +343,21 @@ struct CSVDump:public Callback
         uint64_t      inputScriptSize
     )
     {
-        numTxInputs++;
-        totalTxInput += value;
-        totalBlkInput += value;
+        if (active)
+        {
+            numTxInputs++;
+            totalTxInput += value;
 
-        // outputTxHash
-        uint8_t outputTxHash[1 + 2*kSHA256ByteSize];
-        toHex(outputTxHash, upTXHash);
+            // outputTxHash
+            uint8_t outputTxHash[1 + 2*kSHA256ByteSize];
+            toHex(outputTxHash, upTXHash);
 
-        // Script
-        uint8_t script[1 + 2*inputScriptSize];
-        toHex(script, inputScript, inputScriptSize);
+            // Script
+            uint8_t script[1 + 2*inputScriptSize];
+            toHex(script, inputScript, inputScriptSize);
 
-        fprintf(inputFile, "%" PRIu64 ",%" PRIu64 ",\"%s\",\"%s\",%" PRIu64 "\n", txID, inputIndex, script, outputTxHash, outputIndex);
+            fprintf(inputFile, "%" PRIu64 ",%" PRIu64 ",\"%s\",\"%s\",%" PRIu64 "\n", txID, inputIndex, script, outputTxHash, outputIndex);
+        }
     }
 
     virtual void wrapup()
@@ -325,7 +366,7 @@ struct CSVDump:public Callback
         fclose(inputFile);
         fclose(blockFile);
         fclose(txFile);
-        info("done\n");
+        info("Done\n");
         exit(0);
     }
 };
